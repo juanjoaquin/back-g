ace278cb00bb60cb042d536f6a80a43b74664a28
Juan Joaquin <juanmanueljoaquin@hotmail.com>
2025-12-08 01:14:28 -0300

    struct gorm en el domain

---
+++ internal/user/domain.go
@@ -0,0 +1,15 @@
+package user
+
+import "time"
+
+// 1. Vamos a definir la struct de User con estos campos del Domain, que esta era la capa que interactuaba con la base de datos.
+// Pero evitaba que interactue directamente sin capas entre la db y las request. Esta es la tabla intermedia
+type User struct {
+	ID        string     `json:"id" gorm:"type:char(36);not null;primary_key;unique_index"` // 2. Al usar GORM como ORM, debemos definirle los valores que posee la base de datos. Y sus limites, y demas.
+	FirstName string     `json:"first_name" gorm:"type:char(50);not null"`
+	LastName  string     `json:"last_name" gorm:"type:char(50);not null"`
+	Email     string     `json:"email" gorm:"type:char(50);not null"`
+	Phone     string     `json:"phone" gorm:"type:char(30);not null"`
+	CreatedAt *time.Time `json:"-"`
+	UpdatedAt *time.Time `json:"-"`
+}
